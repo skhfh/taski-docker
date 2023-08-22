3ae899d431f6650c1b35112fded99c354e35d0ce
Шамиль Хамидуллин <shamil.415@yandex.ru>
2023-08-22 20:17:15 +0300

    Test CORS delete

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -21,12 +21,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corsheaders',
+#    'corsheaders',
     'rest_framework',
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
+#    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
